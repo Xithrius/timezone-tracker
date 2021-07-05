d924d4184a2d79300819ebb8c4c7a1fe55b2dbb8
Xithrius <xithrius@gmail.com>
2021-07-04 21:45:25 -0700

    Parsing strings for a timezone offset integer works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, num::ParseIntError};
+
+fn user_input(buffer: &str) -> Result<String, io::Error> {
+    let mut user_input_string = String::new();
+
+    println!("{}", &buffer);
+
+    io::stdin().read_line(&mut user_input_string)?;
+
+    Ok(user_input_string.trim().to_string())
+}
+
+fn parse_timezone_offset(timezone_offset: &str) -> Result<i8, ParseIntError> {
+    Ok(timezone_offset[3..].parse::<i8>().unwrap())
+}
+
+fn main() -> io::Result<()> {
+    let local_timezone: String = user_input("Local timezone offset: ")?;
+    let offset_timezone: String = user_input("another test: ")?;
+
+    println!("\n{}", &local_timezone);
+    println!("{}", &offset_timezone);
+
+    println!("{:?}", parse_timezone_offset(&offset_timezone));
+
+    Ok(())
 }
