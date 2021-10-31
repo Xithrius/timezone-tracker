db180935ef3946d9763c1be514babd788df285e4
Xithrius <xithrius@gmail.com>
2021-10-30 19:46:21 -0700

    Main function is now async

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod handlers;
 mod terminal;
 mod utils;
 
-use anyhow::{Result};
+use anyhow::Result;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     match CompleteConfig::new() {
-        Ok(config) => terminal::draw_terminal_ui(&config),
+        Ok(config) => terminal::draw_terminal_ui(&config).await,
         Err(err) => println!("{}", err),
     };
 
