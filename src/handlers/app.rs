f6f0ac6a01fe76226b6232ec655aa1440533a7f0
Xithrius <xithrius@gmail.com>
2021-10-30 19:45:55 -0700

    App state, database connection, and input window map have been added as attributes

--- src/handlers/app.rs
+++ src/handlers/app.rs
@@ -1,20 +1,56 @@
-use std::collections::VecDeque;
+use std::{
+    collections::{HashMap, VecDeque},
+    path::PathBuf,
+};
+
+use anyhow::{bail, Error, Result};
+use maplit::hashmap;
+use rusqlite::Connection;
+use rustyline::line_buffer::LineBuffer;
+
+use crate::utils::pathing::config_path;
+
+pub enum State {
+    Normal,
+    Input,
+    Help,
+}
 
 #[derive(Debug)]
 pub struct User {
     pub name: String,
-    pub offset: i8,
+    pub offset: i64,
+}
+
+impl User {
+    pub fn new(name: String, offset: i64) -> Self {
+        Self { name, offset }
+    }
 }
 
 pub struct App {
-    /// Users and their timezone offset.
+    /// State of the application
+    pub state: State,
+    /// Users and their timezone offset
     pub timezone_data: VecDeque<User>,
+    /// Connection to the sqlite database
+    pub conn: Connection,
+    /// Boxes to insert text into
+    pub input_map: HashMap<&'static str, LineBuffer>,
 }
 
 impl App {
-    pub fn new() -> App {
-        App {
-            timezone_data: VecDeque::new(),
+    pub fn new() -> Result<Self, Error> {
+        let database_path = config_path("db.sqlite3");
+
+        match Connection::open(&database_path) {
+            Ok(database_connection) => Ok(Self {
+                timezone_data: VecDeque::new(),
+                conn: database_connection,
+                state: State::Normal,
+                input_map: hashmap! {"timezone" => LineBuffer::with_capacity(4096)},
+            }),
+            Err(_) => bail!(rusqlite::Error::InvalidPath(PathBuf::from(&database_path))),
         }
     }
 }
