6213f3d602297d9fafba826c10fec0696a93f1e5
Xithrius <xithrius@gmail.com>
2022-03-18 21:15:46 -0700

    Removed panic_any w/ useless error handling, created more tests

--- src/utils/pathing.rs
+++ src/utils/pathing.rs
@@ -1,21 +1,22 @@
-use std::panic::panic_any;
-
 const BINARY_NAME: &str = env!("CARGO_BIN_NAME");
 
-pub fn config_path(filename: &str) -> String {
+pub fn config_path(file: &str) -> String {
     match std::env::consts::OS {
-        "linux" | "macos" => match std::env::var("HOME") {
-            Ok(env_home_path) => format!("{}/.config/{}/{}", env_home_path, BINARY_NAME, filename),
-            Err(err) => panic_any(err),
-        },
-        "windows" => match std::env::var("APPDATA") {
-            Ok(appdata_path) => format!("{}\\{}\\{}", appdata_path, BINARY_NAME, filename),
-            Err(err) => std::panic::panic_any(err),
-        },
+        "linux" | "macos" => format!(
+            "{}/.config/{}/{}",
+            std::env::var("HOME").unwrap(),
+            BINARY_NAME,
+            file
+        ),
+        "windows" => format!(
+            "{}\\{}\\{}",
+            std::env::var("APPDATA").unwrap(),
+            BINARY_NAME,
+            file
+        ),
         _ => unimplemented!(),
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,27 +24,40 @@ mod tests {
     #[test]
     #[cfg(target_os = "windows")]
     fn test_windows_config_path() {
-        match std::env::var("APPDATA") {
-            Ok(appdata_path) => assert_eq!(
-                config_path("config.toml"),
-                format!("{}\\{}\\{}", appdata_path, BINARY_NAME, "config.toml")
-            ),
-            Err(err) => std::panic::panic_any(err),
-        }
+        assert_eq!(
+            config_path("config.toml"),
+            format!(
+                "{}\\{}\\config.toml",
+                std::env::var("APPDATA").unwrap(),
+                BINARY_NAME
+            )
+        )
     }
 
     #[test]
     #[cfg(any(target_os = "macos", target_os = "linux"))]
     fn test_unix_config_path() {
-        match std::env::var("HOME") {
-            Ok(env_home_path) => assert_eq!(
-                config_path("config.toml"),
-                format!(
-                    "{}/.config/{}/{}",
-                    env_home_path, BINARY_NAME, "config.toml"
-                )
-            ),
-            Err(err) => std::panic::panic_any(err),
-        }
+        assert_eq!(
+            config_path("config.toml"),
+            format!(
+                "{}/.config/{}/config.toml",
+                std::env::var("HOME").unwrap(),
+                BINARY_NAME,
+            )
+        )
+    }
+
+    #[test]
+    #[should_panic]
+    #[cfg(any(
+        target_os = "ios",
+        target_os = "android",
+        target_os = "freebsd",
+        target_os = "dragonfly",
+        target_os = "openbsd",
+        target_os = "netbsd"
+    ))]
+    fn test_ios_config_path() {
+        config_path("config.toml");
     }
 }
